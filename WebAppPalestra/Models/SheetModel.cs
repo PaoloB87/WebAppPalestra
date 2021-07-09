e6f443edc42c95ccdd48d82a2579ecbdd34684b0
Paolo Bacchi <p.bacchi@vetrya.com>
2021-07-09 16:50:58 +0200

    Aggiungere i file di progetto.

---
+++ WebAppPalestra/Models/SheetModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPalestra.Models
+{
+    public class SheetModel
+    {
+        public int Age { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+}
