2b0542762711ef3da1062a871403ab201f01619f
Paolo Bacchi <p.bacchi@vetrya.com>
2021-07-14 17:54:43 +0200

    Login

---
+++ WebAppPalestra/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppPalestra.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VerifyLogin()
+        {
+            return View();
+        }
+    }
+}
